fe41c387fd60fb0e1ef5e9b21e9cc886ffda09dc
Ramadhan <kinyaoedwin@gmail.com>
2024-02-28 11:39:17 +0300

    the .format() method for wrking with strings

--- dirtyhands.py
+++ dirtyhands.py
@@ -1,5 +1,6 @@
 #After familiarising with python basics.....>>
 #this new .format() method can interest you and make your work with strings very very easy. 
+
 #Here is how it works>>
 
 sentence = "I went to {} and bought {} kilos of {}"
