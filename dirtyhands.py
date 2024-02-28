e3a0f3208a2996c7ad2550902b084e46b4fa70b2
Ramadhan <kinyaoedwin@gmail.com>
2024-02-28 11:03:33 +0300

    the .format() method for wrking with strings

---
+++ dirtyhands.py
@@ -0,0 +1,9 @@
+#After familiarising with python basics.....>>
+#this new .format() method can interest you and make your work with strings very very easy. 
+#Here is how it works>>
+
+sentence = "I went to {} and bought {} kilos of {}"
+country = "Columbia"
+weight = "3"
+substance = "Tea leaves"
+print(sentence.format(country, weight, substance))
